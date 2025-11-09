6a4681cbb2480f680c5c4fbb6d4e88755191c215
arnauldmartinez <arnauldmartinez@gmail.com>
2025-11-08 19:51:09 -0800

    Initial Commit

---
+++ visor-llm-test/visor_llm_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  visor_llm_testApp.swift
+//  visor-llm-test
+//
+//  Created by Arnauld Martinez on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct visor_llm_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
